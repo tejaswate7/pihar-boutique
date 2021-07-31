90fda443bb5f98b46eb6cac82a580bb845d1f716
Tejas Wate <tejaswate@Tejass-MacBook-Air.local>
2021-07-31 13:12:54 +0530

    create the basic layout of HomePage component

---
+++ src/homepage.component.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import './homepage.styles.scss'
+
+const HomePage = () => (
+    <div className='homepage'>
+        <div className= 'directory-menu'>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>KURTIS</h1>
+                    <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>SALWAR</h1>
+                    <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>LEHENGAS</h1>
+                    <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>CHAPPAL</h1>
+                    <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1 className='title'>PAITHANI</h1>
+                    <span className='subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+        </div>
+    </div>
+)
+
+export default HomePage;
